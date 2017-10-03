66a18afd8d7b96828a1765ae3570aaf3cc9ef425
Adam Schachne <adamschachne@live.com>
2017-10-02 17:01:49 -0700

    gitignore uncessary files

--- teamchat-files/overwolf-app/settings.js
+++
@@ -1,9 +0,0 @@
-function openSettings() {
-	overwolf.windows.obtainDeclaredWindow("settings", function(cb) {
-
-		overwolf.windows.restore("settings", function(cb) {
-			console.log("settings opened", cb);
-
-		});
-	})
-}
